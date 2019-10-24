a3962cd65fe9c76872c718b63436912b7c78338b
Carlos FG <mercuriocfg@yahoo.com.br>
2019-10-24 20:24:26 -0300

    Projeto spring adicionado. closes #1

---
+++ src/main/java/br/usp/Apprest/ApprestApplication.java
@@ -0,0 +1,13 @@
+package br.usp.Apprest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApprestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/usp/Apprest/ApprestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.usp.Apprest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
